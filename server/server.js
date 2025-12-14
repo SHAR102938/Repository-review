312d1f8a053e451616c0ac7016245fc4b6f78115
SHAR102938 <shahayush102938@gmail.com>
2025-12-14 15:59:19 +0530

    fix: allow server to accept connections from any IP

--- server/server.js
+++ server/server.js
@@ -513,6 +513,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port: ${port}`);
 });
